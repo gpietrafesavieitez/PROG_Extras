ea9c08d473917202685039a71815b9e19592b5bd
Gabriel <43631819+gpietrafesavieitez@users.noreply.github.com>
2018-10-18 17:22:45 +0200

    Update ExtraCirculo.java

--- extraCirculo/src/extracirculo/ExtraCirculo.java
+++ extraCirculo/src/extracirculo/ExtraCirculo.java
@@ -6,16 +6,29 @@ public class ExtraCirculo {
     //public final static float PI = 3.14159265359f;
     public static Scanner leer = new Scanner(System.in);
     
-    //Codifica un programa que calcule la longitud, area y volumen de un círculo
+    //Codifica un programa que calcule la longitud, area y volumen de un cÃ­rculo
     public static void main(String[] args) {
         float r; //Declaro el radio primero porque lo necesita el while a continuacion
         
         do{
-            System.out.print("Introduce el radio del círculo: "); r = leer.nextFloat();
+            System.out.print("Introduce el radio del círculo: ");
+            
+            while(!leer.hasNextInt()){ //No pueden ser letras
+                System.out.print("Introduce el radio del círculo: ");
+                leer.next(); 
+            }
+            
+            r = leer.nextFloat();
             
         }while(r < 0); //No puede ser negativo
         
-        System.out.println("\n- El área del círculo es de " + (Math.PI * Math.pow(r,2))+ " unidades.\n- El volumen del círculo es de " + (4/3 * Math.PI * Math.pow(r,3))  + " unidades.\n- La longitud del círculo es de " + (2 * Math.PI * r) + " unidades.");
+        System.out.println("\n- El área del círculo es de "
+                + (Math.PI * Math.pow(r,2)) 
+                + " unidades.\n- El volumen del círculo es de " 
+                + (4/3 * Math.PI * Math.pow(r,3))  
+                + " unidades.\n- La longitud del círculo es de " 
+                + (2 * Math.PI * r) 
+                + " unidades."
+        );
     } 
 }
-//Esto es una prueba
